fcb876e25248d182415b24c18e5311336dfa16ff
BenCheng2 <bencheng0609@gmail.com>
2023-02-21 17:52:42 -0500

    feat: function to convert jpeg, jpe, jxr, tif and tiff
    Add more functions to convert different formats of images including jpeg, jpe, jxr, tif and tiff
    
    Fixes: None
    Related Issues: None

--- Converter.py
+++ Converter.py
@@ -5,7 +5,6 @@ from PyQt5.QtGui import QPixmap
 from PIL import Image
 
 
-
 class ImageConverter(QWidget):
     file_label: QLabel
     file_load_button: QPushButton
@@ -14,7 +13,12 @@ class ImageConverter(QWidget):
     found_image: str
 
     jpg_button: QPushButton
+    jpeg_button: QPushButton
+    jpe_button: QPushButton
+    jxr_button: QPushButton
     png_button: QPushButton
+    tif_button: QPushButton
+    tiff_button: QPushButton
     bmp_button: QPushButton
 
     def __init__(self):
@@ -29,10 +33,21 @@ class ImageConverter(QWidget):
         self.convert_button_group = QHBoxLayout()
 
         self.jpg_button = QPushButton("Convert to JPG")
-        self.convert_button_group.addWidget(self.jpg_button)
+        self.jpeg_button = QPushButton("Convert to JPEG")
+        self.jpe_button = QPushButton("Convert to JPE")
+        self.jxr_button = QPushButton("Convert to JXR")
         self.png_button = QPushButton("Convert to PNG")
-        self.convert_button_group.addWidget(self.png_button)
+        self.tif_button = QPushButton("Convert to TIF")
+        self.tiff_button = QPushButton("Convert to TIFF")
         self.bmp_button = QPushButton("Convert to BMP")
+
+        self.convert_button_group.addWidget(self.jpg_button)
+        self.convert_button_group.addWidget(self.jpeg_button)
+        self.convert_button_group.addWidget(self.jpe_button)
+        self.convert_button_group.addWidget(self.jxr_button)
+        self.convert_button_group.addWidget(self.png_button)
+        self.convert_button_group.addWidget(self.tif_button)
+        self.convert_button_group.addWidget(self.tiff_button)
         self.convert_button_group.addWidget(self.bmp_button)
 
         layout = QVBoxLayout()
@@ -44,15 +59,21 @@ class ImageConverter(QWidget):
         self.setLayout(layout)
 
         self.file_load_button.clicked.connect(self.open_file)
+
         self.jpg_button.clicked.connect(self.convert_to_jpg)
+        self.jpeg_button.clicked.connect(self.convert_to_jpeg)
+        self.jpe_button.clicked.connect(self.convert_to_jpe)
+        self.jxr_button.clicked.connect(self.convert_to_jxr)
         self.png_button.clicked.connect(self.convert_to_png)
+        self.tif_button.clicked.connect(self.convert_to_tif)
+        self.tiff_button.clicked.connect(self.convert_to_tiff)
         self.bmp_button.clicked.connect(self.convert_to_bmp)
 
     def open_file(self):
         file_dialog = QFileDialog(self)
 
-        file_dialog.setNameFilter("Image files (*.jpg *.png *.bmp)")
-        file_dialog.selectNameFilter("Image files (*.jpg *.png *.bmp)")
+        file_dialog.setNameFilter("Image files (*.jpg *.jpeg *.jpe *.jxr *.png *.tif *.tiff *.bmp)")
+        file_dialog.selectNameFilter("Image files (*.jpg *.jpeg *.jpe *.jxr *.png *.tif *.tiff *.bmp)")
         file_dialog.setDefaultSuffix("jpg")
         file_dialog.setFileMode(QFileDialog.ExistingFile)
         file_dialog.setViewMode(QFileDialog.Detail)
@@ -78,8 +99,24 @@ class ImageConverter(QWidget):
         # By the way, the jpeg works the same way
         self._convert_format_helper("JPG files (*.jpg)", "jpg")
 
+    def convert_to_jpeg(self):
+        self._convert_format_helper("JPEG files (*.jpeg)", "jpeg")
+
+    def convert_to_jpe(self):
+        self._convert_format_helper("JPE files (*.jpe)", "jpe")
+
+    def convert_to_jxr(self):
+        self._convert_format_helper("JXR files (*.jxr)", "jxr")
+
     def convert_to_png(self):
         self._convert_format_helper("PNG files (*.png)", "png")
+
+    def convert_to_tif(self):
+        self._convert_format_helper("TIF files (*.tif)", "tif")
+
+    def convert_to_tiff(self):
+        self._convert_format_helper("TIFF files (*.tiff)", "tiff")
+
     def convert_to_bmp(self):
         self._convert_format_helper("BMP files (*.bmp)", "bmp")
 
